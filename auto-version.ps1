6bc234364e3191c5b16c2172302ae07247c5a833
Krystian Babilinski <kbabilinski@magicleap.com>
2023-08-02 13:08:37 -0400

    update version script

--- auto-version.ps1
+++ auto-version.ps1
@@ -37,9 +37,9 @@ $projectVersionWSpaces = $projectVersion -replace "-", " "
 $config = Get-Content -Path "./docs-versions.json" | ConvertFrom-Json
 
 # Edit the object properties
-$config.versions.current.label = $currentVersionWSpaces
+$config.current.label = $currentVersionWSpaces
 if ($config.PSObject.Properties.Name -contains $projectVersion) {
-    $config.versions.$projectVersion.label = $projectVersionWSpaces
+    $config.$projectVersion.label = $projectVersionWSpaces
 }
 
 # Convert the object back to a JSON string
