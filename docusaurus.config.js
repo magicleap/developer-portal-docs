33d0a52ffcdee97fe5d55052b5c87b56a014aeb2
Krystian Babilinski <kbabilinski@magicleap.com>
2023-09-13 22:45:47 -0400

    Improve image quality

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -293,8 +293,8 @@ module.exports = {
     [
       'ideal-image',
       {
-        quality: 70,
-        max: 1030, // max resized image's size.
+        quality: 80,
+        max: 1920, // max resized image's size.
         min: 640, // min resized image's size. if original is lower, use that size.
         steps: 2, // the max number of images generated between min and max (inclusive)
         // disableInDev: false,
